2cd21175b7a38c64dfe0ce391052ccbf7bdb0415
Sriram Anne <sriram.anne2@gmail.com>
2022-01-07 08:42:20 -0500

    Base commit

---
+++ changemaker/src/main/java/com/codeproject/changemaker/ChangemakerApplication.java
@@ -0,0 +1,13 @@
+package com.codeproject.changemaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChangemakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChangemakerApplication.class, args);
+	}
+
+}
---
+++ changemaker/src/test/java/com/codeproject/changemaker/ChangemakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeproject.changemaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChangemakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
